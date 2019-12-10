bba157d321ba3123b0a7ce2f78f0544a222a4656
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-11 00:48:44 +0200

    Updates

---
+++ precache-manifest.f20b881f81d02cfe7fc9350cffbed0c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7d08233fa4f7801dafb801fec9a8f3",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "501204337507884f732b",
+    "url": "/react_add-todo-form/static/css/main.b5b15881.chunk.css"
+  },
+  {
+    "revision": "8c1ca4089b4f1d17e5c4",
+    "url": "/react_add-todo-form/static/js/2.44edda1a.chunk.js"
+  },
+  {
+    "revision": "501204337507884f732b",
+    "url": "/react_add-todo-form/static/js/main.63ed899f.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
